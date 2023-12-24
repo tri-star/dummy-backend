aece5f347997db4828048d6977d1847f3ffe64de
tristar <tristar.2nd@gmail.com>
2023-12-24 23:25:07 +0900

    Supabaseの通信にかかる時間をXRayのトレース情報に追加

--- src/domain/users/api/user-api.ts
+++ src/domain/users/api/user-api.ts
@@ -1,6 +1,7 @@
 import { supabase } from '@libs/supabase/api-client'
 import { type User, dbUserSchema } from '../user'
 import dayjs from 'dayjs'
+import { createSegment, traceAsync } from '@libs/xray-tracer'
 
 export type UserListResponse = {
   data: User[]
@@ -9,32 +10,37 @@ export type UserListResponse = {
 
 /**
  * ユーザー一覧を取得する
- * @param _
  */
 export async function fetchUsers(): Promise<UserListResponse> {
-  const dbUserList = await supabase.from('users').select('*')
+  const segment = createSegment('Supabase')
 
-  if (dbUserList.error != null) {
-    throw new Error(JSON.stringify(dbUserList.error))
-  }
+  const users = await traceAsync<User[]>(segment, 'query', async () => {
+    const dbUserList = await supabase.from('users').select('*')
 
-  const users: User[] =
-    dbUserList.data
-      ?.map((dbUserJson) => {
-        const parseResult = dbUserSchema.safeParse(dbUserJson)
-        if (!parseResult.success) {
-          console.error(parseResult.error.errors)
-          return undefined
-        }
-        return {
-          id: parseResult.data.id,
-          name: parseResult.data.name,
-          email: parseResult.data.email,
-          createdAt: dayjs(parseResult.data.created_at).toDate(),
-          updatedAt: dayjs(parseResult.data.updated_at).toDate(),
-        } as User
-      })
-      .filter((user): user is User => user !== undefined) ?? []
+    if (dbUserList.error != null) {
+      throw new Error(JSON.stringify(dbUserList.error))
+    }
+
+    const users: User[] =
+      dbUserList.data
+        ?.map((dbUserJson) => {
+          const parseResult = dbUserSchema.safeParse(dbUserJson)
+          if (!parseResult.success) {
+            console.error(parseResult.error.errors)
+            return undefined
+          }
+          return {
+            id: parseResult.data.id,
+            name: parseResult.data.name,
+            email: parseResult.data.email,
+            createdAt: dayjs(parseResult.data.created_at).toDate(),
+            updatedAt: dayjs(parseResult.data.updated_at).toDate(),
+          } as User
+        })
+        .filter((user): user is User => user !== undefined) ?? []
+
+    return users
+  })
 
   return {
     data: users,
@@ -43,16 +49,20 @@ export async function fetchUsers(): Promise<UserListResponse> {
 }
 
 export async function createUser(user: User): Promise<void> {
-  const dbUser = dbUserSchema.parse({
-    id: user.id,
-    name: user.name,
-    password: '',
-    email: user.email,
-    created_at: user.createdAt?.toISOString(),
-    updated_at: user.updatedAt?.toISOString(),
+  const segment = createSegment('Supabase')
+
+  await traceAsync(segment, 'insert', async () => {
+    const dbUser = dbUserSchema.parse({
+      id: user.id,
+      name: user.name,
+      password: '',
+      email: user.email,
+      created_at: user.createdAt?.toISOString(),
+      updated_at: user.updatedAt?.toISOString(),
+    })
+    const result = await supabase.from('users').insert(dbUser)
+    if (result.error != null) {
+      throw new Error(JSON.stringify(result.error))
+    }
   })
-  const result = await supabase.from('users').insert(dbUser)
-  if (result.error != null) {
-    throw new Error(JSON.stringify(result.error))
-  }
 }
---
+++ src/libs/xray-tracer.ts
@@ -0,0 +1,32 @@
+import * as AWSXRay from 'aws-xray-sdk-core'
+
+export function createSegment(name: string) {
+  return new AWSXRay.Segment(name)
+}
+
+export async function traceAsync<T>(
+  segment: AWSXRay.Segment,
+  traceName: string,
+  callback: () => Promise<T>,
+): Promise<T> {
+  segment.addNewSubsegment(traceName)
+
+  const result = await new Promise<T>((resolve, reject) => {
+    void AWSXRay.captureAsyncFunc(
+      traceName,
+      async (subsegment) => {
+        try {
+          const result = await callback()
+          resolve(result)
+        } catch (e) {
+          subsegment?.addError(e as Error)
+          reject(e)
+        } finally {
+          subsegment?.close()
+        }
+      },
+      segment,
+    )
+  })
+  return result
+}
