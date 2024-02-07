8e606491c0ca1c9446d1f510dd3b8f63fede73f9
tristar <tristar.2nd@gmail.com>
2024-02-08 04:03:39 +0900

    テスト前にテーブルをクリアするように変更

--- src/functions/admin/users/tests/admin-user-handler.test.ts
+++ src/functions/admin/users/tests/admin-user-handler.test.ts
@@ -1,6 +1,11 @@
+import { supabase } from '@libs/supabase/api-client'
 import { createAdminUserNoAuthHandler } from '../handler'
 
 describe('admin-user-handler', () => {
+  beforeEach(async () => {
+    await supabase.from('admin_users').delete()
+  })
+
   test('登録処理が成功すること', async () => {
     const result = await createAdminUserNoAuthHandler({
       name: 'test',
