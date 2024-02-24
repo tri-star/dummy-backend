f8e56ffcaa00af1f5be799d117be1e3053878fbd
tristar <tristar.2nd@gmail.com>
2024-02-25 00:39:25 +0900

    細かな修正

--- src/functions/admin/users/handler.ts
+++ src/functions/admin/users/handler.ts
@@ -8,7 +8,7 @@ import {
 } from '@/domain/admin-users/admin-user'
 import { ulid } from 'ulid'
 import z from 'zod'
-import { type AdminApiContext, middyfyWithAdminAuth } from '@libs/lambda'
+import { middyfyWithAdminAuth } from '@libs/lambda'
 import { type APIGatewayProxyEvent } from 'aws-lambda'
 import { fetchAdminUserList } from '@/domain/admin-users/api/fetch-admin-user-list'
 
@@ -57,24 +57,22 @@ export type ListAdminUsersResponse = z.infer<typeof listAdminUsersResponseSchema
 /**
  * 管理者一覧
  */
-export const listAdminUsersHandler = middyfyWithAdminAuth(
-  async (event: APIGatewayProxyEvent, context: AdminApiContext) => {
-    try {
-      const parsed = listAdminUsersRequestSchema.safeParse(JSON.parse(event.body ?? '{}'))
-      if (!parsed.success) {
-        return formatJSONUserErrorResponse({ error: parsed.error })
-      }
-      const { loginId /* page, offset */ } = parsed.data
-
-      const userListResponse = await fetchAdminUserList(loginId)
-      return formatJSONResponse({
-        success: true,
-        data: userListResponse?.list ?? [],
-        count: userListResponse?.count ?? 0,
-      } satisfies ListAdminUsersResponse)
-    } catch (e) {
-      console.error(e)
-      return formatJSONUserErrorResponse({ error: e })
+export const listAdminUsersHandler = middyfyWithAdminAuth(async (event: APIGatewayProxyEvent) => {
+  try {
+    const parsed = listAdminUsersRequestSchema.safeParse(JSON.parse(event.body ?? '{}'))
+    if (!parsed.success) {
+      return formatJSONUserErrorResponse({ error: parsed.error })
     }
-  },
-)
+    const { loginId /* page, offset */ } = parsed.data
+
+    const userListResponse = await fetchAdminUserList(loginId)
+    return formatJSONResponse({
+      success: true,
+      data: userListResponse?.list ?? [],
+      count: userListResponse?.count ?? 0,
+    } satisfies ListAdminUsersResponse)
+  } catch (e) {
+    console.error(e)
+    return formatJSONUserErrorResponse({ error: e })
+  }
+})
