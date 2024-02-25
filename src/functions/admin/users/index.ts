d505edc47d8b8d39ae38712512ae1d09b3bc960c
tristar <tristar.2nd@gmail.com>
2024-02-25 21:46:34 +0900

    管理者ユーザー作成APIを追加

--- src/functions/admin/users/index.ts
+++ src/functions/admin/users/index.ts
@@ -1,18 +1,38 @@
 import type { AWS } from '@serverless/typescript'
 import { handlerPath } from '@libs/handler-resolver'
 import { corsSettings } from '@functions/cors'
+import { createAdminUserSchema } from './schema'
 
 export const rules: AWS['functions'] = {
   createAdminUserNoAuthHandler: {
-    handler: `${handlerPath(__dirname)}/handlers/create-admin-user-no-auth-handler/createAdminUserNoAuthHandler`,
+    handler: `${handlerPath(__dirname)}/handlers/create-admin-user-no-auth-handler.createAdminUserNoAuthHandler`,
     events: [
       {
         sns: 'createAdminUser',
       },
     ],
   },
+  createAdminUserHandler: {
+    handler: `${handlerPath(__dirname)}/handlers/create-admin-user-handler.createAdminUserHandler`,
+    events: [
+      {
+        http: {
+          method: 'post',
+          path: 'admin/users',
+          request: {
+            schemas: {
+              'application/json': {
+                schema: createAdminUserSchema,
+              },
+            },
+          },
+          cors: corsSettings,
+        },
+      },
+    ],
+  },
   listAdminUsersHandler: {
-    handler: `${handlerPath(__dirname)}/handlers/list-admin-users-handler/listAdminUsersHandler`,
+    handler: `${handlerPath(__dirname)}/handlers/list-admin-users-handler.listAdminUsersHandler`,
     events: [
       {
         http: {
