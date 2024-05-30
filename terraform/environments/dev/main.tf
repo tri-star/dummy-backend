c2b02cd5e6a9cf16c758ce3f84fd83f369c3669e
tristar <tristar.2nd@gmail.com>
2024-05-31 00:01:57 +0900

    Lintエラー修正

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,9 +1,9 @@
 module "ssm" {
-  stage             = "dev"
-  supabase_url      = var.supabase_url
-  supabase_anon_key = var.supabase_anon_key
+  stage                     = "dev"
+  supabase_url              = var.supabase_url
+  supabase_anon_key         = var.supabase_anon_key
   supabase_service_role_key = var.supabase_service_role_key
-  app_key           = var.app_key
+  app_key                   = var.app_key
 
   source = "../../modules/ssm"
 }
