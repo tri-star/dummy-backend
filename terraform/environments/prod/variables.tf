a3881378a0b791fd7b681383a48f084846738797
tristar <tristar.2nd@gmail.com>
2024-06-13 00:31:38 +0900

    本番環境用設定の追加

---
+++ terraform/environments/prod/variables.tf
@@ -0,0 +1,19 @@
+variable "supabase_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "supabase_anon_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "supabase_service_role_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "app_key" {
+  type      = string
+  sensitive = true
+}
